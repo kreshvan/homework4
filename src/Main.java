22363c299a044493b3e22966f21602231b9cbafa
днс <kreshvan12317@yandex.ru>
2024-05-11 22:57:25 +0300

    homework 444

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+
+//задание 1
+        int A =500;
+        System.out.println("Значение переменной A с типом int равно"+" "+A);
+        byte B = 100;
+        System.out.println("Значение переменной B с типом byte равно"+" "+B);
+        short C = 1000;
+        System.out.println("Значение переменной C с типом short равно"+" "+C);
+        long D = 3000;
+        System.out.println("Значение переменной D с типом long равно"+" "+D);
+        float Y = 3.4f;
+        System.out.println("Значение переменной Y с типом float равно"+" "+Y);
+        double F = 1.5;
+        System.out.println("Значение переменной F с типом short равно"+" "+F);
+        //задание 2
+        float a = 27.12f;
+        long b = 987678965549l;
+        double t = 2.786;
+        int r = 569;
+        short q = -159;
+        char w = 27897;
+        byte v = 67;
+        //задание 3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    }
+}
